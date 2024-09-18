fffd0e9bed996b5bf1fb7d82439c95743d197267
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-18 08:18:25 +0630

    db update

--- database/migrations/2024_02_23_163516_create_game_type_product_table.php
+++ database/migrations/2024_02_23_163516_create_game_type_product_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->integer('rate');
             $table->timestamps();
 
-            $table->foreign('game_type_id')->references('id')->on('game_types')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+           // $table->foreign('game_type_id')->references('id')->on('game_types')->onDelete('cascade');
+           // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
 
         });
     }
