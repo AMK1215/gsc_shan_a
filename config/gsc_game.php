748e94ca136de5754ddd6ce23aeff17c0cf65da9
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-22 08:25:55 +0630

    gsc_updated by amk

--- config/gsc_game.php
+++ config/gsc_game.php
@@ -3,7 +3,7 @@
 return [
 
     'api' => [
-        'operator_code' => env('GSC_SEAMLESS_OPERATOR_ID'),  // Using SEAMLESS_OPERATOR_ID from .env
+        'operator_code' => env('GSC_SEAMLESS_OPERATOR_CODE'),  // Using SEAMLESS_OPERATOR_ID from .env
         'password' => env('GSC_SEAMLESS_PASSWORD'),          // Using SEAMLESS_PASSWORD from .env (if required)
         'secret_key' => env('GSC_SEAMLESS_SECRET_KEY'),      // Using SEAMLESS_SECRET_KEY from .env
         'url' => env('GSC_SEAMLESS_API_URL'),                // Using SEAMLESS_API_URL from .env
